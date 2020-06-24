ef68470e66ddf920091584605db096aeaa49ab4b
Kesia <miiizx1@gmail.com>
2020-06-24 17:50:33 -0300

    Att lib

---
+++ LojaVirtual/src/testejfx/LoginBoundary.java
@@ -0,0 +1,14 @@
+package testejfx;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class LoginBoundary extends Application {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
